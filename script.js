15cfd6ffede8afe80c456bcab5f39ad6de0b0d81
Mitchel Bower <mitchelbower@outlook.com>
2020-02-06 13:32:31 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// hello... I've been waiting...
+
+let vsCodeIsWack = true;
+
+if (vsCodeIsWack === true) {
+  console.log(`damn, vs code is wack...`);
+} else {
+  console.log(`hello i've been waiting...`);
+}
